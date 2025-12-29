f2ad1090a1b1038306eb19afaf94c5458279b316
Apoorva Deshpande <apoorva.deshpande1995@gmail.com>
2025-12-29 15:22:59 +0530

    Updated JSAlerts

--- src/test/java/Pages/JSAlerts.java
+++ src/test/java/Pages/JSAlerts.java
@@ -5,19 +5,25 @@ import java.time.Duration;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class JSAlerts {
 	protected WebDriver driver;
+	protected WebDriverWait wait;
 	
 	public JSAlerts(WebDriver driver) {
 		super();
 		this.driver = driver;
+		wait = new WebDriverWait(driver,Duration.ofSeconds(20));
 	}
 
 	private By clickonJSAlert = By.xpath("//button[text()='Click for JS Alert']");
 	private By JSAlert = By.xpath("//a[text()='JavaScript Alerts']");
 	private By clickOnJSConfirm = By.xpath("//button[text()='Click for JS Confirm']");
 	private By result = By.id("result");
+	private By clickOnJSPrompt = By.xpath("//button[text()='Click for JS Prompt']");
 	
 	
 	public void valdiateAlerts() {
@@ -40,6 +46,18 @@ public class JSAlerts {
 		alert.dismiss();
 		System.out.println();
 		System.out.println("Alert result "+driver.findElement(result).getText());		
+	}
+	
+	
+	public void validateJSPrompt() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(clickOnJSPrompt ));
+		driver.findElement(clickOnJSPrompt).click();
+		Alert alert = driver.switchTo().alert();
+		System.out.println("Alert message : "+alert.getText());
+		alert.sendKeys("Apoorva here!!!!");
+		alert.accept();
+		System.out.println("Alert result : "+driver.findElement(result).getText());
+		
 	}
 
 }
